cd436c2b44d41a54c9759eb8596313a3efec6530
Andres <andresjml@gmail.com>
2021-09-27 21:23:38 -0400

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts('Seeding')
+10.times do
+    Route.create(name:Faker::Address.community)
+end
+
+60.times do
+    Client.create(name:Faker::Commerce.vendor, route_id:(1..10).to_a.sample, address:Faker::Address.full_address)
+end
+
+200.times do
+    Order.create(volume:(1..13500).to_a.sample, client_id:(1..60).to_a.sample)
+end
+
+10.times do
+    TransportCompany.create(name:Faker::Company.name)
+end
+
+30.times do
+    Vehicle.create(type:'53 ft trailer w/tractor', capacity:13500, transport_company_id:(1..10).to_a.sample)
+end
+
+5.times do
+    Trip.create(vehicle_id:(1..30).to_a.sample, order_id:(1..200).to_a.sample, status:false)
+end
+
+puts('Seeding completed!!')
