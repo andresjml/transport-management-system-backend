1e5d1d3ec04686deb98046d251a56d92aa8c1cf5
Andres <andresjml@gmail.com>
2021-09-29 13:52:18 -0400

    display data modified on vehicle serializer

--- app/serializers/vehicle_serializer.rb
+++ app/serializers/vehicle_serializer.rb
@@ -1,4 +1,4 @@
 class VehicleSerializer < ActiveModel::Serializer
-  attributes :id, :v_type, :capacity
-  has_one :transport_company
+  attributes :id, :v_type, :capacity, :transport_company
+  #has_one :transport_company
 end
