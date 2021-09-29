67fe1160eb38b79c37cd32a48760bf2f7643ff63
Andres <andresjml@gmail.com>
2021-09-29 08:09:11 -0400

    adding rescue_from on applicationController

--- app/models/transport_company.rb
+++ app/models/transport_company.rb
@@ -1,3 +1,4 @@
 class TransportCompany < ApplicationRecord
     has_many :vehicles
+    has_many :trips, through: :vehicles
 end
--- app/serializers/transport_company_serializer.rb
+++ app/serializers/transport_company_serializer.rb
@@ -1,3 +1,3 @@
 class TransportCompanySerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :trips
 end
