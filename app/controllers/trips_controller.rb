b2dd355a556831dd12f0e908328e74b1227ee6a4
Andres <andresjml@gmail.com>
2021-09-29 08:56:23 -0400

    full CRUD trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,4 +4,36 @@ class TripsController < ApplicationController
         trips = Trip.all
         render json: trips
     end
+
+    def show
+        trip= find_trip
+        render json:trip
+    end
+
+    def create
+        trip=Trip.create!(trip_params)
+        render json:trip
+    end
+
+    def update
+        trip= find_trip
+        trip.update!(trip_params)
+        render json:trip
+    end
+
+    def destroy
+        trip= find_trip
+        trip.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_trip
+        Trip.find(params[:id])
+    end
+
+    def trip_params
+        params.permit(:vehicle_id, :order_id, :status)
+    end
 end
