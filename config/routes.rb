41293d147deee6a19e43b9313938578331c20c1f
Andres <andresjml@gmail.com>
2021-09-29 08:27:02 -0400

    only index/show in routes, clients and orders routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :trips, only: [:index]
   resources :vehicles, only: [:index]
   resources :transport_companies, only: [:index]
-  resources :orders, only: [:index]
-  resources :clients, only: [:index]
-  resources :routes, only: [:index]
+  resources :orders, only: [:index, :show]
+  resources :clients, only: [:index, :show]
+  resources :routes, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
