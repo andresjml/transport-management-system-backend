91e1ac0ac9981e64ee4398fc68fe74b1dd2cf148
Andres <andresjml@gmail.com>
2021-09-29 09:10:48 -0400

    full CRUD vehicles

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -4,4 +4,36 @@ class VehiclesController < ApplicationController
         vehicles = Vehicle.all
         render json: vehicles
     end
+
+    def show
+        vehicle= find_vehicle
+        render json:vehicle
+    end
+
+    def create
+        vehicle=Vehicle.create!(vehicle_params)
+        render json:vehicle
+    end
+
+    def update
+        vehicle= find_vehicle
+        vehicle.update!(vehicle_params)
+        render json:vehicle
+    end
+
+    def destroy
+        vehicle= find_vehicle
+        vehicle.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_vehicle
+        Vehicle.find(params[:id])
+    end
+
+    def vehicle_params
+        params.permit(:v_type, :capacity, :transport_company_id)
+    end
 end
