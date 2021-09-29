8042ec456d90717e07781881af2f00e179f4de6d
Andres <andresjml@gmail.com>
2021-09-29 08:52:06 -0400

    full CRUD transport_companies

--- app/controllers/transport_companies_controller.rb
+++ app/controllers/transport_companies_controller.rb
@@ -4,4 +4,36 @@ class TransportCompaniesController < ApplicationController
         transport_companies = TransportCompany.all
         render json: transport_companies
     end
+
+    def show
+        transport_company= find_transport_company
+        render json:transport_company
+    end
+
+    def create
+        transport_company=TransportCompany.create!(transport_company_params)
+        render json:transport_company
+    end
+
+    def update
+        transport_company= find_transport_company
+        transport_company.update!(transport_company_params)
+        render json:transport_company
+    end
+
+    def destroy
+        transport_company= find_transport_company
+        transport_company.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_transport_company
+        TransportCompany.find(params[:id])
+    end
+
+    def transport_company_params
+        params.permit(:name)
+    end
 end
