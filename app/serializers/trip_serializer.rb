fece5379d0c61df0d74f67382dc9ff87d0fa021d
Andres <andresjml@gmail.com>
2021-09-30 22:19:11 -0400

    modifying :order to trips serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,5 +1,5 @@
 class TripSerializer < ActiveModel::Serializer
   attributes :id, :status, :vehicle_id, :vehicle, :transport_company, :order
   #has_one :vehicle
-  #has_one :order
+  has_one :order
 end
