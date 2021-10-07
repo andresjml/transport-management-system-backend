16ac96eb16dee56b768a516fb3448a6114aa85c1
Andres <andresjml@gmail.com>
2021-10-07 11:23:51 -0400

    added assigned to vehicle serializer

--- app/serializers/vehicle_serializer.rb
+++ app/serializers/vehicle_serializer.rb
@@ -1,4 +1,4 @@
 class VehicleSerializer < ActiveModel::Serializer
-  attributes :id, :v_type, :capacity, :transport_company
+  attributes :id, :v_type, :capacity, :transport_company, :assigned
   #has_one :transport_company
 end
