7ed6aa5e6949e87aa6154970afb1cb89dc8a9a80
Andres <andresjml@gmail.com>
2021-10-07 09:51:50 -0400

    adding status column to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_004639) do
+ActiveRecord::Schema.define(version: 2021_10_07_134711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_09_28_004639) do
     t.bigint "client_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status"
     t.index ["client_id"], name: "index_orders_on_client_id"
   end
 
