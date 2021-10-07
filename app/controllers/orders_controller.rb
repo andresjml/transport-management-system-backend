e5182d8c95969a1f3da3060612a33d1327a49518
Andres <andresjml@gmail.com>
2021-10-07 10:08:40 -0400

    adding update to order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,11 +8,21 @@ class OrdersController < ApplicationController
         order= find_order
         render json:order
     end
+
+    def update
+        order= find_order
+        order.update!(order_params)
+        render json:order
+    end
       
     
     private
     
     def find_order
         Order.find(params[:id])
+    end
+
+    def order_params
+        params.permit(:status)
     end
 end
