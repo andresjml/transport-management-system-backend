29cf102a389c70aa6564bd4923f97e6933dfe489
Andres <andresjml@gmail.com>
2021-10-07 09:53:20 -0400

    adding update route  to orders table

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :trips, only: [:index, :show, :create, :update, :destroy]
   resources :vehicles, only: [:index, :show, :create, :update, :destroy]
   resources :transport_companies, only: [:index, :show, :create, :update, :destroy]
-  resources :orders, only: [:index, :show]
+  resources :orders, only: [:index, :show, :update]
   resources :clients, only: [:index, :show]
   resources :routes, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
